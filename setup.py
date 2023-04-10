fb07c515c551d912c39bad5f0b12fa0f6a2b5616
sarata00 <118468090+sarata00@users.noreply.github.com>
2023-04-10 18:26:18 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
-from distutils.core import setup, find_packages
+from distutils.core import setup
+from setuptools import find_packages
 
-setup(name="ProjectName",
+setup(name="Bind-Pred",
       version="1.0",
       description="Description of my project",
-      long_description=open('README.md').read()
+      long_description=open('README.md').read(),
       author="Alexandre Casadesús, Laia Fortuny and Sara Tolosa Alarcón",
       author_email="saratolosaalarcon@gmail.com",
+      #package_dir="app/modules",
       packages=find_packages(where = "app"),
       install_requires =[
-        "bipython >= 1.81; python_version == 3.10.8",
+        "biopython >= 1.81",
         "pandas >= 2.0", 
         "scikit-learn", 
         "bs4", 
